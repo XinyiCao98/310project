9282abd8f9e4460c49b61a0387b36fde8ffa8dd1
shixi_xinyi18 <shixi_xinyi18@staff.weibo.com>
2019-09-27 22:40:37 -0700

    // Add some invalid checks (fixed format)

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -70,7 +70,6 @@ export default class InsightFacade implements IInsightFacade {
             });
         });
     }
-
     private checkInput(id: string, kind: InsightDatasetKind) {
         // check whether dataset ID is in right format
         if (!id || id.length === 0) {
@@ -88,7 +87,6 @@ export default class InsightFacade implements IInsightFacade {
             throw new InsightError("ID is incorrect InsightDatasetKind.");
         }
     }
-
     // Check the details of whether a section has all features
     private checkValidDataset(allJFile: any) {
         for (const singleCourse of allJFile) {
@@ -103,26 +101,16 @@ export default class InsightFacade implements IInsightFacade {
                 // Log.trace("222");
                 try {
                     // Log.trace("333");
-                    if (typeof singleSection.Subject === "string"
-                        && typeof singleSection.Course === "string"
-                        && typeof singleSection.Avg === "number"
-                        && typeof singleSection.Professor === "string"
-                        && typeof singleSection.Title === "string"
-                        && typeof singleSection.Pass === "number"
-                        && typeof singleSection.Fail === "number"
-                        && typeof singleSection.Audit === "number"
-                        && typeof singleSection.id === "number"
-                        && typeof singleSection.Year === "string") {
-                        const dept = singleSection.Subject;
-                        const id = singleSection.Course;
-                        const avg = singleSection.Avg;
-                        const instructor = singleSection.Professor;
-                        const title = singleSection.Title;
-                        const pass = singleSection.Pass;
-                        const fail = singleSection.Fail;
-                        const audit = singleSection.Audit;
-                        const uuid = singleSection.id.toString;
-                        const year = parseInt(singleSection.Year, 10);
+                    if (typeof singleSection.Subject === "string" && typeof singleSection.Course === "string"
+                        && typeof singleSection.Avg === "number" && typeof singleSection.Professor === "string"
+                        && typeof singleSection.Title === "string" && typeof singleSection.Pass === "number"
+                        && typeof singleSection.Fail === "number" && typeof singleSection.Audit === "number"
+                        && typeof singleSection.id === "number" && typeof singleSection.Year === "string") {
+                        const dept = singleSection.Subject; const id = singleSection.Course;
+                        const avg = singleSection.Avg; const instructor = singleSection.Professor;
+                        const title = singleSection.Title; const pass = singleSection.Pass;
+                        const fail = singleSection.Fail; const audit = singleSection.Audit;
+                        const uuid = singleSection.id.toString; const year = parseInt(singleSection.Year, 10);
                         const validSec = new Map<string, number | string>([
                             ["courses_dept", dept], ["courses_id", id],
                             ["courses_avg", avg], ["courses_instructor", instructor],
@@ -138,7 +126,6 @@ export default class InsightFacade implements IInsightFacade {
             }
         }
     }
-
     public removeDataset(id: string): Promise<string> {
         // check whether dataset ID is in right format
         if (!id || id.length === 0) {
@@ -170,7 +157,6 @@ export default class InsightFacade implements IInsightFacade {
             });
         });
     }
-
     public performQuery(query: any): Promise<any[]> {
         let Validornot = this.CheckQuery(query);
         let Output: string[] = ["KAIWEN"];
@@ -179,7 +165,6 @@ export default class InsightFacade implements IInsightFacade {
         }
         return Promise.resolve(Output);
     }
-
     public listDatasets(): Promise<InsightDataset[]> {
         return new Promise<InsightDataset[]>((fulfill, reject) => {
             let presentList: any[] = [];
@@ -249,7 +234,6 @@ export default class InsightFacade implements IInsightFacade {
             return false;
         }
     }
-
     // Check the properties from Column are in given information or not
     public CheckCol(Col: string[]): boolean {
         let i: number = 0;
@@ -264,16 +248,13 @@ export default class InsightFacade implements IInsightFacade {
             return false;
         }
     }
-
-    // Check if element inside order is valid
-    public CheckOrd(Col: string[], Ord: string): boolean {
+    public CheckOrd(Col: string[], Ord: string): boolean { // Check if element inside order is valid
         if (Col === null || Col.indexOf(Ord) < 0) {
             return false;
         } else {
             return true;
         }
     }
-
     // Check Compare Statement without logic
     public CheckCWL(ItemInComparator: any): boolean {
         if (!this.queryOrNot(ItemInComparator)) {
@@ -288,11 +269,8 @@ export default class InsightFacade implements IInsightFacade {
             return false;
         }
         return true;
-
     }
-
-    // Check Is Statement without logic
-    public CheckIWL(ItemInComparator: any): boolean {
+    public CheckIWL(ItemInComparator: any): boolean { // Check Is Statement without logic
         if (!this.queryOrNot(ItemInComparator) || Object.keys(ItemInComparator).length !== 1) {
             return false;
         }
@@ -306,7 +284,6 @@ export default class InsightFacade implements IInsightFacade {
         }
         return true;
     }
-
     public CheckL(ItemInComparator: any): boolean { // check AND || OR logic
         if (ItemInComparator === null || !Array.isArray(ItemInComparator) || ItemInComparator.length < 1) {
             return false;
@@ -319,5 +296,4 @@ export default class InsightFacade implements IInsightFacade {
             return true;
         }
     }
-// tslint:disable-next-line:max-file-line-count
 }
